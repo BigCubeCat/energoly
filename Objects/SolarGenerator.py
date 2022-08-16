4058fd16149adf0a2bd60aedd9358fdb82c6bd73
BigCubeCat <bigcubecat@yandex.ru>
2022-08-16 23:25:03 +0700

    пойду спать

--- Objects/SolarGenerator.py
+++ Objects/SolarGenerator.py
@@ -1,13 +1,27 @@
 from .Types import Generator
-
+from .utils import constrain
+from math import pi
 
 class SolarGenerator(Generator):
     def __init__(self, index, parents, name, rent_price) -> None:
         super().__init__(index, parents, name)
         self.setBill(rent_price)
-        self.inertia = 0 # Инерция Солнечки (ЛОЛ?) 
 
     def update(self):
         # TODO
+        func_sin = lambda x, day_tick=48: 0.3 * sin(x * 4*pi/day_tick) - 3.5
+        """
+        spread = lambda s, variable_value=0.3: constrain(s + uniform(-variable_value/2, variable_value/2), 0, 15)
+
+        inverse_coordinates = lambda x, y: (19-x, 10-y)
+        first_sun = lambda x, y: - 0.102206143125*y**2+1.088911573499*y - 0.05493164125*x**2+0.6005859425*x + 5.518798830625019
+        second_sun = lambda x, y: - 0.102206143125*y**2+1.088911573499*y - 0.05493164125*x**2+0.6005859425*x + 5.939941414375021
+        dependence_on_the_position = lambda x, y: max(first_sun(x, y), second_sun(*inverse_coordinates(x, y)))
+
+        gen_sun = lambda weather_sun, tick, x, y: spread(weather_sun + func_sin(tick) + (dependence_on_the_position(x, y)-dependence_on_the_position(2, 3)))
+
+        s = [gen_sun(select[tick], tick, *(2, 3)) for tick in range(0, 100)]
+        print(s)
+        """
         pass
 
--- Objects/SteamGenerator.py
+++ Objects/SteamGenerator.py
@@ -1,5 +1,5 @@
 from .Types import Generator
-
+from .utils import constrain
 
 class SteamGenerator(Generator):
     def __init__(self, index, parents, name, rent_price) -> None:
@@ -7,17 +7,14 @@ class SteamGenerator(Generator):
         self.setBill(rent_price)
         self.previousPower = 0
 
-    def constrain(self, value, min_, max_):
-        return min(max_, max(min_, value))
-    
     def efficiency_TPS(self, count_fuel, a=-1/128, b=1/8, c=0.4):
-        return a*count_fuel**2 + b*count_fuel + c
+        return a*count_fuel ** 2 + b * count_fuel + c
     
     def inertia_TPS(self, prew_power, factor=0.6, decrease=0.5): 
         return factor * (prew_power - decrease)
 
     def power_TPS(self, prew_power, count_fuel): 
-        return count_fuel*self.constrain(self.efficiency_TPS(count_fuel), 0.4, 0.9) + self.inertia_TPS(prew_power)
+        return count_fuel * constrain(self.efficiency_TPS(count_fuel), 0.4, 0.9) + self.inertia_TPS(prew_power)
 
     def update(self, power):
 

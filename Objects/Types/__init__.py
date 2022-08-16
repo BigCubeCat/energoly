3aced97f679df7d5c084736a2afda620ff22ac49
BigCubeCat <bigcubecat@yandex.ru>
2022-08-16 21:59:32 +0700

    Objects

---
+++ Objects/Types/__init__.py
@@ -0,0 +1,5 @@
+from .Consumer import Consumer
+from .Edge import Edge
+from .Generator import Generator
+from .Station import Station
+
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 #! python
 import random
+import Types
+from Objects.WindGenerator import *
 
-from Types.Consumer import Consumer
-from Types.Edge import Edge
-from Types.Station import Station
+station = Types.Station(index=0, parents=[], name="main", criticalDamage=50);
+edge = Types.Edge(index=1, parent=station.index, criticalDamage=5)
+factory = Types.Consumer(index=2, parents=[1], name="fA")
 
-station = Station(index=0, parents=[], name="main", criticalDamage=50);
-edge = Edge(index=1, parent=station.index, criticalDamage=5)
-factory = Consumer(index=2, parents=[1], name="fA")
 
 ## Типа аукцион
 factory.setBill(10)
 if __name__ == "__main__":
     for i in range(100):
         factory.update(random.randint(1, 10))
-    print()
     print(factory.totatBill)
 

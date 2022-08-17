bdeac23cbb4cd903efe6610059907e695284020c
LuckichS <llukih05@gmail.com>
2022-08-17 17:24:07 +0300

    добавил outputsCOunt для станций и объекты станций

---
+++ Objects/Stations.py
@@ -0,0 +1,15 @@
+from Types.Station import Station
+
+
+class MainStation(Station):
+	def __init__(self, index, parents, name, criticalDamage) -> None:
+		super().__init__(index, parents, name, criticalDamage, 3)
+
+class StationA(Station):
+	def __init__(self, index, parents, name, criticalDamage) -> None:
+		super().__init__(index, parents, name, criticalDamage, 3)
+
+	
+class StationB(Station):
+	def __init__(self, index, parents, name, criticalDamage) -> None:
+		super().__init__(index, parents, name, criticalDamage, 2)
--- Objects/Types/Station.py
+++ Objects/Types/Station.py
@@ -2,8 +2,8 @@ from .BaseObject import BaseObject
 
 
 class Station(BaseObject):
-    def __init__(self, index, parents, name, criticalDamage) -> None:
+    def __init__(self, index, parents, name, criticalDamage, outputsCount) -> None:
         super().__init__(index, parents, name)
         self.criticalDamage = criticalDamage
         self.currentDamage = 0
-
+		self.outputsCount = outputsCount

9201b227ed0b1b98714c735a0475011eaf75dc0e
BigCubeCat <bigcubecat@yandex.ru>
2022-08-18 18:27:42 +0700

    FIX TabError

--- Objects/Battery.py
+++ Objects/Battery.py
@@ -2,7 +2,6 @@ from .Types import Consumer
 from .Types import Generator
 
 class Battery(Consumer, Generator):
-	def __init__(self, index, parent, name):
-		super().__init__(index, parent, name)
-	
-		self.energyCount = 0
+    def __init__(self, index, parent, name):
+        super().__init__(index, parent, name)
+        self.energyCount = 0

55f8a0355885e5410e31585a3ad18e838f5c2105
BigCubeCat <bigcubecat@yandex.ru>
2022-08-19 16:03:03 +0700

    edge looses simulation

--- Objects/Types/Edge.py
+++ Objects/Types/Edge.py
@@ -1,13 +1,24 @@
-from .BaseObject import BaseObject
+if __name__ == "__main__":
+    from BaseObject import BaseObject
+else:
+    from .BaseObject import BaseObject
 
 
 class Edge(BaseObject):
     def __init__(self, index, parent) -> None:
         super().__init__(index, [parent], "edge")
-        self.criticalDamage = 100 # in percents
+        self.criticalDamage = 100  # in percents
         self.currentDamage = 0
 
-    def update(energy):
-        pass # TODO: Сделать симуляцию нагрузки
+    def update(self, energy, stations):
+        abs_energy = abs(energy)
+        # Считаем потери
+        looses = min(30, abs_energy)
+        looses_percent = (looses / 30)
+        total_looses = abs_energy * looses_percent * 0.25
+        energy -= total_looses
+        # Добавляем энергии на станцию
+        stations[self.parents[0]].add_energy(energy)
+        pass  # TODO: Сделать симуляцию нагрузки
 
 
